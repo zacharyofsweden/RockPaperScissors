c335db12878e94fa3712831730a8fc4c9d8355b0
zacharyofsweden <zacharyofsweden@gmail.com>
2024-10-21 20:45:05 +0100

    Display score and uppdates based on lost or won

--- script.js
+++ script.js
@@ -3,27 +3,38 @@ let humanScore = 0;
 let computerScore = 0;
 let isTheSame = false;
 let roundCount = 0;
-
+let humanIsRoundWinner = false;
+let computerIsRoundWinner = false;
 //Dom 
 
 //Create the 3 buttons for the user to chose from
-const buttonRock = document.createElement('button')
-
-const buttonPapper = document.createElement('button')
-
-const buttonSciscor = document.createElement("button")
-
-const scorKeeper = document.createElement("div")
-
 const divContainer = document.querySelector(".container")
 
+const buttonRock = document.createElement('button')
+const buttonPapper = document.createElement('button')
+const buttonSciscor = document.createElement("button")
+
+
+const scorKeeper = document.createElement("div")
+const playingScore = document.createElement("h1")
+
+//Append
+scorKeeper.appendChild(playingScore)
+divContainer.appendChild(scorKeeper)
 divContainer.appendChild(buttonRock)
 divContainer.appendChild(buttonPapper)
 divContainer.appendChild(buttonSciscor)
-divContainer.appendChild(scorKeeper)
 
-scorKeeper.style.color("red")
-scorKeeper.style.width("100px")
+
+//Displaying score and user choice
+playingScore.textContent = test();
+
+
+// Styling
+
+buttonRock.textContent = "Rock"
+buttonPapper.textContent = "Paper"
+buttonSciscor.textContent = "Scissor"
 
 //Functions 
 
@@ -63,38 +74,44 @@ function getComputerChoice() {
 }
 
 
-
-//Implement a funcktion for picking for user. 
+function test() {
+    let showScore = "It's a tie! : User score " + humanScore + " - " + computerScore
+    return showScore
+}
 
 
 //Displays the current count and who won
 function roundMessage(humanWonRound, computerwonround) {
 
     if (isTheSame == true) {
-        console.log("It's a tie! : User score " + humanScore + " - " + computerScore);
+        playingScore.textContent =  test()
     }
 
     else if (humanWonRound == true) {
         humanScore++;
-        console.log("you won Score is : User score " + humanScore + " - " + computerScore);
+        playingScore.textContent = test()
+        
     }
     else if (computerwonround == true) {
         computerScore++;
-        console.log("you lost Score is : User score " + humanScore + " - " + computerScore);
+        playingScore.textContent = test()
     }
     humanWonRound = false;
     computerwonround = false;
+
 }
 
 //Logic to use the choices and check who won 
 function playRound(humanChoice, computerchoice) {
     //Variabals for the roundFunction, 
     isTheSame = false;
-    let humanIsRoundWinner = false;
-    let computerIsRoundWinner = false;
+    humanIsRoundWinner = false;
+    computerIsRoundWinner = false;
     //Todo Try to figure out a easier solution to check winner
     console.log("You picked: " + humanChoice)
     console.log("The Computer picked: " + computerchoice)
+
+
     if (humanChoice == computerchoice) {
         isTheSame = true
         roundMessage(humanIsRoundWinner, computerIsRoundWinner);
